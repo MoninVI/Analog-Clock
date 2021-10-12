2e59489a3354f052202c317f2c3dcff9842f3b31
MoninVI <monin.vi.96@gmail.com>
2021-10-12 21:18:24 +0300

    Значение времени

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-var date = new Date();
-var hours = date.getHours();
-var minutes = date.getMinutes();
-var seconds = date.getSeconds();
-var widget = hours + ':' + minutes + ':' + seconds;
-document.querySelector('.time').innerHTML = widget;
+function getTime() {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    var widget = hours + ':' + minutes + ':' + seconds;
+    return widget;
+};
+
+function updateTime(widget) {
+    document.querySelector('.time').innerHTML = widget;
+};
+
+setInterval(() => {
+    var valueTime = getTime();
+    updateTime(valueTime);
+}, 1000);
